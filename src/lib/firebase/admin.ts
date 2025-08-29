b647f3dda00106bf07fd702ecd7d5b4c380a33c1
adityapai05 <adityapai.main@gmail.com>
2025-08-29 23:47:59 +0530

    fix: update environment variable for Firebase service account initialization

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -2,7 +2,7 @@ import { getApps, initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
 if (!getApps().length) {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT!);
+  const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_KEY!);
   initializeApp({
     credential: cert(serviceAccount),
   });
